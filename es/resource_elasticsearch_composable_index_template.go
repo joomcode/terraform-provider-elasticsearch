2a772d78f2477475dcc40e18c1c40d0676f4e595
Phillip Baker <phillbaker@retrodict.com>
2022-01-11 15:39:12 -0500

    wip

--- es/resource_elasticsearch_composable_index_template.go
+++ es/resource_elasticsearch_composable_index_template.go
@@ -91,6 +91,7 @@ func resourceElasticsearchComposableIndexTemplateRead(d *schema.ResourceData, me
 
 func elastic7GetIndexTemplate(client *elastic7.Client, id string) (string, error) {
 	res, err := client.IndexGetIndexTemplate(id).Do(context.TODO())
+	log.Printf("[INFO] Index template %+v %+v", t, err)
 	if err != nil {
 		return "", err
 	}
